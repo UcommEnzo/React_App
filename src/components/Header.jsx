7f97a18e525ef512e6ef75e3c15ed5e0398f5198
Aleksey <talkin2007@mail.ru>
2020-05-04 10:32:09 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+    return <header className='header'>
+        <img src='https://pngimg.com/uploads/donut/donut_PNG98.png'/>
+    </header>
+
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Profile() {
+    return <div className='content'>
+        <div>
+            <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'/>
+        </div>
+        <div>
+            ava + opisanie
+        </div>
+        <div>
+            my posts
+            <div>
+                new post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
