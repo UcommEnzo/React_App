5c3f15645225a23cfc62746775c130a4dbc32b83
Aleksey <talkin2007@mail.ru>
2020-05-22 23:12:37 +0300

    add dialogs ava

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import c from './Dialogs.module.css';
-import {NavLink} from "react-router-dom";
 import DialogsItem from "./DialogsItem/DialogsItem";
 import Messages from "./Messages/Messages";
 
 function Dialogs(props) {
     let dialogsElements = props.state.dialogs.map(d => <DialogsItem name={d.name} id={d.id}/>)
     let messagesElements = props.state.messages.map(m => <Messages message={m.message}/>)
+    let avatarsElements = props.state.userAvatars.map(ava => <img className={c.qwe} src={ava.url}/>)
 
     return <div className={c.dialogs}>
+        <div className={c.avatars}>
+            {avatarsElements}
+        </div>
         <div className={c.dialogsItem}>
             {dialogsElements}
         </div>
