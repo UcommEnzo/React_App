9bfb68dc75cecdb2dc71a97c121f20e8a439edec
Aleksey <talkin2007@mail.ru>
2020-10-21 18:50:13 +0300

    cookie login

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ import {Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = (props) => {
     return (
         <div className='app-wrapper'>
-            <Header/>
+            <HeaderContainer/>
             <Navbar/>
             <div className='app-wrapper-content'>
                 <Route path='/Profile/:userId?' render={() => <ProfileContainer store={props.store}/>}/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import c from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-function Header() {
+let Header = (props) => {
     return <header className={c.header}>
         <img src='https://pngimg.com/uploads/donut/donut_PNG98.png'/>
+        <div className={c.loginBlock}>
+            {props.isAuth ? props.login :
+            <NavLink to={'/Login'}>Login</NavLink>}
+        </div>
     </header>
-
 }
 export default Header;
