e58ebfabd4e5ea0302d5a5f5fc20dcb06574c2d0
Aleksey <talkin2007@mail.ru>
2020-10-30 13:11:55 +0300

    Profilestatus setState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import c from './ProfileInfo.module.css';
 import Preloader from "../../Common/Preloader/Preloader";
-import nj from "../../../assets/images/needjob.png"
-import dnj from "../../../assets/images/dontneedjob.png"
+import needJob from "../../../assets/images/needjob.png"
+import dontNeedJob from "../../../assets/images/dontneedjob.png"
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
     if (!props.profile)
@@ -18,9 +19,12 @@ const ProfileInfo = (props) => {
             <div className={c.Block2}>Обо мне: {props.profile.aboutMe}</div>
             <div className={c.Block3}>Контакты: {props.profile.contacts.vk}</div>
             <div><p>Поиск работы:
-            <img className={c.Block4} src={props.profile.lookingForAJob ? nj : dnj}/></p>
+            <img className={c.Block4} src={props.profile.lookingForAJob ? needJob : dontNeedJob}/></p>
             Подробнее: {props.profile.lookingForAJobDescription}
             </div>
+        </div>
+        <div>
+            <ProfileStatus status={"First status"}/>
         </div>
     </div>
 }
