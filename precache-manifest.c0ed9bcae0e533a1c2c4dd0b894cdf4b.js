fe185dc46c413825787ee7ed4a6b500c98188706
Aleksey <talkin2007@mail.ru>
2020-11-14 00:08:55 +0300

    Updates

---
+++ precache-manifest.c0ed9bcae0e533a1c2c4dd0b894cdf4b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdcb0e8fd3bb157cc5c47761bfbaae90",
+    "url": "/Test_1/index.html"
+  },
+  {
+    "revision": "cc2c74bc54c177127104",
+    "url": "/Test_1/static/css/3.1f21673d.chunk.css"
+  },
+  {
+    "revision": "52faa9edd14414eb8980",
+    "url": "/Test_1/static/css/4.7e8873fe.chunk.css"
+  },
+  {
+    "revision": "0e922b3dd9aec68bbe75",
+    "url": "/Test_1/static/css/main.2a6c435c.chunk.css"
+  },
+  {
+    "revision": "4fd2307b4b1df57812b5",
+    "url": "/Test_1/static/js/2.3143417b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Test_1/static/js/2.3143417b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc2c74bc54c177127104",
+    "url": "/Test_1/static/js/3.1a5c6099.chunk.js"
+  },
+  {
+    "revision": "52faa9edd14414eb8980",
+    "url": "/Test_1/static/js/4.582bfd8f.chunk.js"
+  },
+  {
+    "revision": "0e922b3dd9aec68bbe75",
+    "url": "/Test_1/static/js/main.425d3649.chunk.js"
+  },
+  {
+    "revision": "2e81fff1827fb013a4f3",
+    "url": "/Test_1/static/js/runtime-main.ab94fb18.js"
+  },
+  {
+    "revision": "c355b938d0bd0d9da6b1c5b3e51e630b",
+    "url": "/Test_1/static/media/dontneedjob.c355b938.png"
+  },
+  {
+    "revision": "5ade8ab1787e0b5f327064bdd6368c6d",
+    "url": "/Test_1/static/media/preloader.5ade8ab1.svg"
+  }
+]);
